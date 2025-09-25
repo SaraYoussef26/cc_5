ffbfcbeddfbab4bc698fb11f94a4275fcbeeeaab
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-09-25 08:37:01 -0400

    Created an array

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+//Step 2
+let employees = [
+    {name: "Matthew", hourlyRate: 20, hoursWorked: 40},
+    {name: "Ashley", hourlyRate: 15, hoursWorked: 42},
+    {name: "Martin", hourlyRate: 23, hoursWorked: 44},
+    {name: "Gina", hourlyRate: 25, hoursWorked: 38},
+]
