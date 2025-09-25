891cccd3590e78df66624815e9064e19ca22e1de
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-09-25 09:11:29 -0400

    added base pay functions

--- cc_5.js
+++ cc_5.js
@@ -3,5 +3,15 @@ let employees = [
     {name: "Matthew", hourlyRate: 20, hoursWorked: 40},
     {name: "Ashley", hourlyRate: 15, hoursWorked: 42},
     {name: "Martin", hourlyRate: 23, hoursWorked: 44},
-    {name: "Gina", hourlyRate: 25, hoursWorked: 38},
-]
+    {name: "Gina", hourlyRate: 25, hoursWorked: 38}
+];
+//Step 3
+function calculateBasePay (rate, hours) {
+let regularHours;
+if (hours > 40) {
+    regularHours = 40;
+} else {
+    regularHours = hours;
+}
+return regularHours * rate; 
+}
